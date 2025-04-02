7942cc430ee9ea72cecd22e4a53bddeda2a907cb
tiago-silver <tiagopereirasilva5425@gmail.com>
2025-04-01 22:43:10 -0300

    create sessions controller

--- src/controllers/tables-sesseions-controller.ts
+++ src/controllers/tables-sesseions-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import {knex} from "@/database/knex";
 import { z } from "zod"
+import { AppError } from "@/utils/AppError";
 
 class TablesSessionsController {
     async index(request:Request, response:Response, next:NextFunction){
@@ -9,6 +10,18 @@ class TablesSessionsController {
                 table_id : z.number(),
             })
             const {table_id} = bodySchema.parse(request.body)
+
+            // Validção para evitar duplicidade
+            const session = await knex<TableSessionsRepository>("tables_sessions")
+            .select()
+            .where({table_id})
+            .orderBy("opened_at", "desc")
+            .first()
+            
+            // Verifica se a mesa está aberta
+            if(session && !session.closed_at){
+                throw new AppError("This table is already open!")
+            }
 
             await knex<TableSessionsRepository>("tables_sessions").insert({
                 table_id,
